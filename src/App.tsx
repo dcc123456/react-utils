26536d192dec7ad703a7397fdb76cfab23761a1b
chanx <1243304602@qq.com>
2026-01-05 10:15:23 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Avatar } from "./components/avatar";
+
+function App() {
+  return (
+    <>
+      <div>
+        <Avatar name="Jane Doe" />
+      </div>
+    </>
+  );
+}
+
+export default App;
